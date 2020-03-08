59ef888c68fe993cf9f94cb9319b863c6364449d
Richell Bonfield <richell.bonfield12@gmail.com>
2020-03-08 05:05:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // Add your functions here
 
-function map(src, cb) {
+function map(src, callbackF) {
   let arr = []
 
   for (let i = 0; i < src.length; i++) {
     let element = src[i]
-    arr.push(cb(element))
+    arr.push(callbackF(element))
   }
 
   return arr;
 }
 
-function reduce(src, cb, starting){
-  let r = (starting) ? starting : src[0]
-  let i = (starting) ? 0 : 1
+function reduce(src, callbackF, startingP){
+  let arr = (startingP) ? startingP : src[0]
+  let i = (startingP) ? 0 : 1
 
   for (; i < src.length; i++) {
     r = cb(src[i], r)
