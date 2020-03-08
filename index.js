4405d18f3a7703d6c6c70bfff9b13f6771a50a73
Richell Bonfield <richell.bonfield12@gmail.com>
2020-03-08 05:06:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+
+function map(src, callbackF) {
+  let arr = []
+
+  for (let i = 0; i < src.length; i++) {
+    let element = src[i]
+    arr.push(callbackF(element))
+  }
+
+  return arr;
+}
+
+function reduce(src, callbackF, startingP){
+  let arr = (startingP) ? startingP : src[0]
+  let i = (startingP) ? 0 : 1
+
+  for (i; i < src.length; i++) {
+    arr = callbackF(src[i], arr)
+  }
+
+  return arr;
+}
